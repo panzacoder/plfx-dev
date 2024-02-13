59af6a5ebebf92aac2f360fd2a7629b7d85a3120
Jake Hebert <jacobshebert@gmail.com>
2024-02-13 12:28:23 -0600

    Fill out past work

---
+++ app/things/project-card.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Link } from "@/components/ui/link";
+import { ExternalLink } from "lucide-react";
+
+export type ProjectCardProps = {
+  title: string;
+  link?: string;
+  tags: string[];
+  children: React.ReactNode;
+};
+export function ProjectCard({ title, link, tags, children }: ProjectCardProps) {
+  return (
+    <Card className="my-2">
+      <CardHeader className="flex flex-col gap-2">
+        <CardTitle className="flex flex-row gap-2">
+          {title}
+          {link && (
+            <Link href={link}>
+              <ExternalLink />
+            </Link>
+          )}
+        </CardTitle>
+        <div className="flex flex-wrap gap-2">
+          {tags.map((tag: string) => (
+            <Badge key={tag} variant="outline">
+              {tag}
+            </Badge>
+          ))}
+        </div>
+      </CardHeader>
+
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+}
